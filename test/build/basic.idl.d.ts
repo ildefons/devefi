279604201cfc994b3a8ffaefa1e7d543cc8a367c
Ildefons Magrans de Abril <ildefons.magrans@gmail.com>
2024-09-17 18:01:50 +0200

    update

--- test/build/basic.idl.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-import type { IDL } from '@dfinity/candid';
-
-export interface _anon_class_2_1 { 'test' : ActorMethod<[], bigint> }
-export interface _SERVICE extends _anon_class_2_1 {}
-export declare const idlFactory: IDL.InterfaceFactory;
-export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- test/build/basic.idl.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  const _anon_class_2_1 = IDL.Service({ 'test' : IDL.Func([], [IDL.Nat], []) });
-  return _anon_class_2_1;
-};
-export const init = ({ IDL }) => {
-  return [IDL.Record({ 'ledgerId' : IDL.Principal })];
-};
